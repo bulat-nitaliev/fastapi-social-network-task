b09590560af781a1598ddac3d51381f83eb7d2eb
Bulat <nibulat924@gmail.com>
2023-07-31 03:25:42 +0300

    user_router

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List, Optional
 from src.auth.schemas import UserRead
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
@@ -17,8 +17,20 @@ async def get_all_users(session:AsyncSession=Depends(get_async_session)):
     result = await session.execute(query)
     return result.all()
 
-@user_router.get('/{user_id}',response_model=UserRead)
+@user_router.get('/{user_id}',response_model=List[UserRead])
 async def get_user(user_id:int,session:AsyncSession=Depends(get_async_session)):
     query = select(user).where(user.c.id==user_id)
     result = await session.execute(query)
-    return result.all()
+    value = result.fetchall()
+    if value:
+        return value
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="User not found")
+
+@user_router.delete('/{user_id}')
+async def delete_user(user_id:int,session:AsyncSession=Depends(get_async_session)):
+    del_user = delete(user).where(user.c.id==user_id)
+    await session.execute(del_user)
+    await session.commit()
+    return f'the user by ID = {user_id} has been deleted'
